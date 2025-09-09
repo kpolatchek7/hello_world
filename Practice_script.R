c189c6c91f41719b80e7b28d9f7eef134f4ab6f4
wendymiyazaki <wmcollege2020@gmail.com>
2025-09-09 09:46:25 -0400

    Script for practice plot

---
+++ Practice_script.R
@@ -0,0 +1,10 @@
+# Create plot
+ggplot(data = data_lionfish,
+       mapping = aes(x = depth_m, y = total_length_mm)) +
+  geom_point(shape = 21, fill = "hotpink", size = 2)
+
+p <- ggplot(data = data_lionfish,
+       mapping = aes(x = depth_m, y = total_length_mm)) +
+  geom_point(shape = 21, fill = "hotpink", size = 2)
+
+ggsave(plot = p, filename = "first_plot.png")
